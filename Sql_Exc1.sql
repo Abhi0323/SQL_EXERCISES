8b3e3e26e96272e72983c067cda7eb6a45f256e8
Abhishek Chandragiri <112967999+Abhi0323@users.noreply.github.com>
2023-04-08 22:37:18 -0500

    Update Sql_Exc1.sql

--- Sql_Exc1.sql
+++ Sql_Exc1.sql
@@ -1,30 +1,36 @@
 1.	List all the people in the passenger table, including their name, itinerary number, fare, and confirmation number. Order by name and fare.
+
 SELECT pas_name,pas_itinerary_no,pas_fare,pas_confirm_no FROM passenger
 ORDER BY pas_name,pas_fare
 
 2.	Using an “OR” operator, list pilot name, state, zip code, and flight pay for pilots who make more than $2,500 per flight and live in either the state TX or AZ. Order by pay in descending order.
+
 SELECT pil_pilotname,pil_state,pil_city,pil_zip,pil_flight_pay FROM pilots
 WHERE pil_flight_pay > 2500 AND (pil_state = 'TX' OR pil_state = 'AZ')
 ORDER BY pil_flight_pay DESC
 
 3. Using an “IN”, list pilot names, zip and flight pay for pilots who make more than $2,500 per flight and live in either the state TX or AZ. Order by pay in descending order.
+
 SELECT Pil_Pilotname, pil_state, pil_city, pil_zip, pil_flight_pay FROM pilots 
 WHERE Pil_flight_pay > 2500 AND pil_state IN  ('TX','AZ')
 ORDER BY Pil_flight_pay DESC
 
 4. Using a SET OPERATOR, list pilot names, zip and flight pay for pilots who make more than $2,500 per flight and live in either the state TX or AZ. Order by pay in descending order.
+
 SELECT Pil_Pilotname, pil_state, pil_city, pil_zip, pil_flight_pay FROM pilots WHERE Pil_flight_pay > 2500
 INTERSECT
 SELECT Pil_Pilotname, pil_state, pil_city, pil_zip, pil_flight_pay FROM pilots WHERE pil_state IN  ('TX','AZ')
 ORDER BY Pil_flight_pay DESC
 
 5.	Using an “AND” and an “OR”, list all information (Equipment Number, Equipment Type, Seat Capacity, Fuel Capacity, and Miles per Gallon) on aircraft that have a seat capacity greater than 300, or aircraft that have a miles per gallon greater than 3.5 miles per gallon and fuel capacity less than 2500. Order by EQ_EQUIP_No.
+
 SELECT * FROM equip_type 
 WHERE eq_seat_capacity > 300 OR 
 (EQ_Miles_Per_Gal > 3.5 AND EQ_Fuel_Capacity < 2500)
 ORDER BY Eq_Equip_No
 
 6.	Using a SET OPERATION, list all information (Equipment Number, Equipment Type, Seat Capacity, Fuel Capacity, and Miles per Gallon) on aircraft that have a seat capacity greater than 300, or aircraft that have a miles per gallon greater than 3.5 miles per gallon and fuel capacity less than 2500. Order by EQ_EQUIP_No.
+
 SELECT * FROM equip_type WHERE eq_seat_capacity > 300
 UNION
 SELECT * FROM equip_type WHERE EQ_Miles_Per_Gal > 3.5 AND EQ_Fuel_Capacity < 2500
